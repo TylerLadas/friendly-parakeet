99bbb4fef467564cc50c8e850c5ecce68438ae0b
TylerLadas <tylerladas@hotmail.com>
2021-06-04 09:56:35 -0400

    add variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,15 @@
 // Assignment code here
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const special = "!@#$%^&*_+=";
+
+const passwordLength = window.input("Choose length of password between 8 and 128 characters.");
+const includeLower = window.prompt("Do you want to use lowercase letters?");
+const includeUpper = window.prompt("Do you want to use uppercase letters?");
+const includeNumbers = window.prompt("Do you want to include numbers?");
+const includeSpecial = window.prompt("Do you want to include special characters?")
+
 
 
 // Get references to the #generate element
