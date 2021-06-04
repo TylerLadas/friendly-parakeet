de59c1b2dcd669c4dbdc4c3e90e6e3d5b53a3419
TylerLadas <tylerladas@hotmail.com>
2021-06-04 11:35:22 -0400

    add confirm and alert boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,49 @@
 // Assignment code here
-const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const special = "!@#$%^&*_+=";
 
-const passwordLength = window.input("Choose length of password between 8 and 128 characters.");
-const includeLower = window.prompt("Do you want to use lowercase letters?");
-const includeUpper = window.prompt("Do you want to use uppercase letters?");
-const includeNumbers = window.prompt("Do you want to include numbers?");
-const includeSpecial = window.prompt("Do you want to include special characters?")
+// password array variables //
+const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const special = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
 
 
+// declare variables //
+let passwordLength = "";
+let includeLower;
+let includeUpper;
+let includeNumbers; 
+let includeSpecial;
+
+
+// prompt how many characters //
+function generatePassword() {
+  passwordLength = window.prompt("Choose length of password between 8 and 128 characters");
+
+  // check password length //
+  while (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8 and 128 characters, try again.");
+    passwordLength = window.prompt("Choose length of password between 8 and 128 characters");
+  }
+
+  // confirm variables to include in password //
+  includeLower = confirm("Click OK to use lowercase letters?");
+  includeUpper = confirm("Click OK to use uppercase letters?");
+  includeNumbers = confirm("Click OK to include numbers?");
+  includeSpecial = confirm("Click OK to include special characters?");
+
+    // if none chosen //
+    while (includeLower === false && includeUpper === false && includeNumbers === false && includeSpecial === false) {
+      window.alert("At least one type of character must be slected!");
+      includeLower = confirm("Click OK to use lowercase letters?");
+      includeUpper = confirm("Click OK to use uppercase letters?");
+      includeNumbers = confirm("Click OK to include numbers?");
+      includeSpecial = confirm("Click OK to include special characters?");
+    }
+
+  // select array items for password //
+  
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
